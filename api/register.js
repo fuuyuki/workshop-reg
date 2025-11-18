9ca0d931874a4c932f2db54c944b9f44c5f1cb21
Muhammad Fikra Adzaky <fikraadzaky123@gmail.com>
2025-11-18 12:46:07 +0700

    add simple register.js

--- api/register.js
+++ api/register.js
@@ -1,7 +1,4 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).send('Method not allowed');
-  }
-  res.status(200).send('Register endpoint works!');
+// api/hello.js
+export default function handler(req, res) {
+  res.status(200).send("Hello World from Vercel!");
 }
-
