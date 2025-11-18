381f954e159baa07a5aac8c3421c99fb73b12737
Muhammad Fikra Adzaky <fikraadzaky123@gmail.com>
2025-11-18 16:56:12 +0700

    add emoji

--- api/confirm.js
+++ api/confirm.js
@@ -29,6 +29,7 @@ export default async function handler(req, res) {
         <main>
           <h2>Halo, ${data.name}</h2>
           <p>Kamu terdaftar dengan nomor <strong>${data.number}</strong>.</p>
+          <p>Tukarkan QR Code dengan Snack dari Panitia🍟</p>
           <p><a href="/">Kembali ke form pendaftaran</a></p>
         </main>
       </body>
